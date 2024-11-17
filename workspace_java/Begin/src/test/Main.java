f58daa20ceeda3166082d15b1b5281ffbb9f1e2b
KYEek <dltnstls89@hotmail.com>
2024-11-17 18:44:45 +0900

    백준 연습 함

--- workspace_java/Begin/src/test/Main.java
+++ workspace_java/Begin/src/test/Main.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("\n⁙⁙⁙⁙⁙시작메뉴 [연규영]님 로그인 중...⁙⁙⁙⁙⁙⁙ \n"
-				+ "1. 로그아웃  2. 회원탈퇴  3. 나의정보보기  4.모든회원정보 \n");
-		System.out.println("⁙".repeat(66));
+		Scanner sc = new Scanner(System.in);
+		
+		int repeat = Integer.parseInt(sc.nextLine()) ;	
+		for(int i =0; i<repeat; i++) {
+			
+		}
+		sc.close();
 	}
 
 }
+	
