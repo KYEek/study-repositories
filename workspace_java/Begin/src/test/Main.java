4ee0034ec0be723e7d290ad9593d8553ba395df1
KYEek <dltnstls89@hotmail.com>
2024-11-19 00:51:20 +0900

    백준 연습

--- workspace_java/Begin/src/test/Main.java
+++ workspace_java/Begin/src/test/Main.java
@@ -5,14 +5,24 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		int repeat = Integer.parseInt(sc.nextLine()) ;	
-		for(int i =0; i<repeat; i++) {
-			
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int count = Integer.parseInt(sc.nextLine()) ;
+		int[] num_arr = new int[count];
+		
+		for(int i = 0; i < num_arr.length; i++) {
+			String str = sc.nextLine();
+			String[] str_arr = str.split(" ");
+			if(str_arr.length == 2) {
+				num_arr[i] = Integer.parseInt(str_arr[0]) + Integer.parseInt(str_arr[1]);
+			}
 		}
-		sc.close();
+		
+		for(int i = 0; i < num_arr.length; i++) {
+			sb.append("Case #"+ (i+1) + ": " + num_arr[i] + "\n");
+		}
+		System.out.println(sb.toString());
 	}
 
 }
-	
