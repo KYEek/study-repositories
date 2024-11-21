81dbf40db2bc7f51ef182becbab1d8145b19063e
KYEek <dltnstls89@hotmail.com>
2024-11-21 17:39:23 +0900

    2024-11-21 수업 정리

--- workspace_jsp/jQueryStudy/src/main/webapp/01_eventHandling/chap08_prepend_append_empty_remove/js/prepend_append_empty_remove.js
+++ workspace_jsp/jQueryStudy/src/main/webapp/01_eventHandling/chap08_prepend_append_empty_remove/js/prepend_append_empty_remove.js
@@ -7,9 +7,21 @@ $(document).ready(function () {
     "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 <span style='color: red; font-weight: bold;'>우리나라</span> 만세";
   const lovecountrysong2 =
     "무궁화 삼천리 화려강산 대한사람 대한으로 <span style='color: blue; font-weight: bold;'>길이보전</span> 하세";
-
-  $("#divVtn > span.add").click(function () {
+  let cnt = 0;
+  $("#divBtn > span.add").click(function () {
     cnt++;
-    $("div.dvi1").prepend(); // prepend() 메서드는 선택한 요소의 내부 앞쪽에 새 요소를 추가합니다.
+    console.log(cnt);
+    $("div.dvi1").prepend(`<p>${cnt},${lovecountrysong1}</p>`); // prepend() 메서드는 선택한 요소의 내부 앞쪽에 새 요소를 추가합니다.
+    $("div.dvi2").append(`<p>${cnt},${lovecountrysong2}</p>`);
+  });
+
+  $("#divBtn > span.empty").click(function () {
+    console.log(cnt);
+    $("div#div1").empty(); // empty() 메서드는 선택한 요소의 자식 요소를 모두 제거합니다.
+    $("div#div2").empty();
+  });
+
+  $("#divBtn > span.remove").click(function () {
+    console.log(cnt);
   });
 });
