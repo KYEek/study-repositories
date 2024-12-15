71f6aa8a5d1c2ccb5c0c6daf208710caa61ac90a
KYEek <dltnstls89@hotmail.com>
2024-12-15 14:48:42 +0900

    메인 페이지 수정정

--- workspace_jsp/TestProject/src/main/webapp/js/mainpage.js
+++ workspace_jsp/TestProject/src/main/webapp/js/mainpage.js
@@ -13,18 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
     element.addEventListener("scroll", (e) => {
       //이벤트 객체 안의 li를 찾는다
       const li = element.querySelectorAll("li");
+      const size_of_screen = element.scrollHeight / li.length;
       //   console.log(li);
-      const li_index = Math.floor(element.scrollTop / 1000);
+      const li_index = Math.floor(element.scrollTop / size_of_screen);
       //   console.log(li_index);
       // 스크롤 값의 첫번째 소수점 자리로 만든다
-      const current_place =
-        Number((element.scrollTop / 1000).toFixed(4)) + 0.05;
-      console.log(current_place);
+      const current_place = Number(
+        (element.scrollTop / size_of_screen).toFixed(4)
+      );
+      // console.log(current_place);
       const animation_position = current_place - li_index;
-      const scaleNum = 1.73 - animation_position;
+      const scaleNum = 1.68 - animation_position;
+      // console.log(li_index);
       if (animation_position > 0.7) {
-        console.log("scaneNum : ", scaleNum);
+        // console.log("scaneNum : ", scaleNum);
         li[li_index].style.transform = `scale(${scaleNum})`;
+      } else {
+        li[li_index].style.transform = `scale(1)`;
       }
     });
   });
