c94a450ed9aad4a7525800f2975f8012022cb334
KYEek <dltnstls89@hotmail.com>
2024-12-30 18:58:10 +0900

    오늘 수업중 수정

--- workspace_jsp/TestProject/src/main/webapp/js/payAddress.js
+++ workspace_jsp/TestProject/src/main/webapp/js/payAddress.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
     "#address_info_and_select"
   ); //지정된 주소가 표시되는 div
   const address_list_main = document.querySelector("#address_list_main"); //주소목록이 표시되는 div
+  let html = "";
+  const address_arr = []; //주소목록을 담을 배열
 
   //주소목록을 가져와서 출력해주는 함수
   fetch("json/address.json")
@@ -14,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let address_html = "<ul>";
       for (const item of data) {
         // console.log(item[key]);
+        address_arr.push(item);
         address_html += `<li>
               <div class="address_list_item" data-addr_no ="${item["pk_addr_no"]}">
                 <div class="address_list_item_text">${item["member_name"]}</div>
@@ -34,36 +37,57 @@ document.addEventListener("DOMContentLoaded", function () {
       address_list_main.innerHTML = address_html;
 
       const address_list_item = document.querySelectorAll(".address_list_item"); //주소목록의 각각의 주소
-      console.log(address_list_item);
 
+      //주소목록의 각각의 주소를 클릭했을때 이벤트
       address_list_item.forEach((item) => {
-        console.log(item);
         item.addEventListener("click", (e) => {
           if (e.target.getAttribute("class") != "address_list_item") {
-            console.log(e.target.parentElement.getAttribute("data-addr_no"));
+            // console.log(e.target.parentElement.getAttribute("data-addr_no"));
             sessionStorage.setItem(
               "selected_address_no",
               e.target.parentElement.getAttribute("data-addr_no")
             );
+            address_arr.forEach((item) => {
+              if (
+                e.target.parentElement.getAttribute("data-addr_no") ==
+                item["pk_addr_no"]
+              ) {
+                address_info_and_select.innerHTML = ` <div>${item["addr_address"]}${item["addr_extraaddr"]}</div>
+                    <a href="#">변경</a>`;
+              }
+            });
           } else {
-            console.log(e.target.getAttribute("data-addr_no"));
+            // console.log(e.target.getAttribute("data-addr_no"));
             sessionStorage.setItem(
               "selected_address_no",
               e.target.getAttribute("data-addr_no")
             );
+            address_arr.forEach((item) => {
+              if (e.target.getAttribute("data-addr_no") == item["pk_addr_no"]) {
+                address_info_and_select.innerHTML = ` <div>${item["addr_address"]}${item["addr_extraaddr"]}</div>
+                    <a href="#">변경</a>`;
+              }
+            });
           }
-          // e.target.getAttibute("data-addr_no");
-          // sessionStorage.setItem("selected_address_no", item["pk_addr_no"]);
+          //사이드바 가리기
+          closeBtn.dispatchEvent(new Event("click"));
         });
-      });
+      }); //end of address_list_item.forEach-------------------
     }); //end of fetch
   //이미지가 출력되는 div
   const delivery_product_list = document.querySelector(
     "div#delivery_product_list"
   );
+  //장바구니 목록을 담는 변수
   const basket_list_str = sessionStorage.getItem("basket_item_arry");
   const basket_list = JSON.parse(basket_list_str);
-  console.log(basket_list);
+
+  //장바구니 이미지 목록을 출력
+  basket_list.forEach((item) => {
+    html += `<img src="${item["imgSrc"]}" />`;
+  });
+  delivery_product_list.innerHTML = html;
+
   //주소목록 x버튼 클릭이벤트
   closeBtn.addEventListener("click", (e) => {
     // 사이드 바 가리가가
@@ -75,7 +99,10 @@ document.addEventListener("DOMContentLoaded", function () {
     //사이드바 표시
     addressList.style.display = "block";
   });
-  const basket_item_arry = JSON.parse(
-    sessionStorage.getItem("basket_item_arry")
-  );
+
+  document
+    .querySelector("#basket_footer_next_button")
+    .addEventListener("click", (e) => {
+      this.location.href = "paymentInfo.html";
+    });
 });
