f45d44eb7aaed33d92af9c3a9a198bece242f73c
KYEek <dltnstls89@hotmail.com>
2025-02-11 02:36:47 +0900

    테스트 공부

--- workspace_spring_boot_17/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ workspace_spring_boot_17/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -15,14 +20,20 @@ class DemoApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	@Transactional
 	@Test
 	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
 		
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
+		List<Answer> answerList = q.getAnswerList();
 		
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 		
 	}
 
