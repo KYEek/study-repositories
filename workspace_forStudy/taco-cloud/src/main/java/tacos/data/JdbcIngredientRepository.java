908aff525d98285c5da00a312387ed1cb5605e2a
KYEek <dltnstls89@hotmail.com>
2025-02-19 21:15:55 +0900

    整理

--- workspace_forStudy/taco-cloud/src/main/java/tacos/data/JdbcIngredientRepository.java
+++ workspace_forStudy/taco-cloud/src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -1,13 +1,18 @@
 package tacos.data;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import tacos.Ingredient;
+
 
 @Repository
-public class JdbcIngredientRepository {
+public class JdbcIngredientRepository implements IngredientRepository {
 
 	private JdbcTemplate jdbc;
 	
@@ -15,4 +20,32 @@ public class JdbcIngredientRepository {
 	public JdbcIngredientRepository(JdbcTemplate jdbc) {
 		this.jdbc = jdbc;
 	}
+	
+	public Iterable<Ingredient> findAll(){
+		return jdbc.query("select id, name, type from Ingredient", this::mapRowToIngredient);
+	}
+	
+	public Ingredient findById(String id) {
+		return jdbc.queryForObject(
+				"select id, name, type from Ingredient where id=?",
+				this::mapRowToIngredient, id);
+	}
+	
+	private Ingredient mapRowToIngredient(ResultSet rs, int rowNum) throws SQLException {
+		return new Ingredient(
+				rs.getString("id"),
+				rs.getString("name"),
+				Ingredient.Type.valueOf(rs.getString("type")));
+	}
+
+	
+	@Override
+	public Ingredient save(Ingredient ingredient) {
+		jdbc.update("insert into Ingredient (id, name, type) values (?, ?, ?)",
+				ingredient.getId(),
+				ingredient.getName(),
+				ingredient.getType().toString());
+		return ingredient;
+	}
+	
 }
